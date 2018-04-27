a0cea5b84362e799f1adefc0bf99655a95a05ec4
ray <rayadleon@gmail.com>
2018-04-27 18:54:35 -0500

    programa 10

--- 10_numeros _aleatorio.c
+++ 10_numeros _aleatorio.c
@@ -2,11 +2,12 @@
 /*E-mail:rayadleon@gmail.com               */
 /*Fecha de creacion:24/04/2018             */
 /*Fecha de modificacion:27/04/2018         */
-/*Descripcion:generar valores aleatorios 
+/*Descripcion:generar valores aleatorios
           de tipo flotante entre  0-1000   */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 /*Definicion de constantes*/
 #define   ROW 3       /*numero de filas de la matriz]*/
 #define   COL 3      /*Numero de columnas de la matriz*/
@@ -14,12 +15,17 @@
 int main(void)
 {
   int array[ROW][COL];
+ srand((unsigned)time(NULL));
+
+
   /*inicializar el array con valores enteros aleatorios 0-1000*/
   for (int i = 0; i < ROW; i++) /*ciclo que controla las filas del array*/
    {
 
+         /*asignacion de datos al array*/
      for (int j = 0; j < COL; j++) /*ciclo que controla las  columnas del array*/
      {
+          printf("%d ", array[i][j]);
         /*ASIGANACION DE DATOS AL ARRAY*/
         /*ulizar la funcion scanf para solicitar datos al usurio*/
 
@@ -30,9 +36,9 @@ int main(void)
      }
    }
    /* agregamos color con \x1b[color]*/
-   printf("\x1b[32m Datos aleatorios  de la matriz : array[%d][%d]\n",ROW,COL );
-    printf("\x1b[31m otro mensaje\n" );
-    printf("\x1b[0m otro mensaje\n " );
+   printf("\n\n\x1b[32m Datos aleatorios  de la matriz : array[%d][%d]\n",ROW,COL );
+    printf("\n\n\x1b[31m otro mensaje\n" );
+    printf("\n\n\x1b[0m otro mensaje\n " );
 
     printf("{");
     for (int i = 0; i < ROW; i++) /*ciclo que controla las filas del array*/
